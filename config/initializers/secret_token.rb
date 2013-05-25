c4fc55e18a2834f13800964c0d579df9c05f0159
Elizabeth Thompson <ethompson@ancestry.com>
2013-05-25 11:04:03 -0700

    setup files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuizApp::Application.config.secret_token = 'c95a47148583e5e0e61947cb34c408b32d9a8ea207f28db84e65ba65341dab0ccea3c768026768c7ddca4643bf92b083980103a0009c562fb4658848d89df483'
