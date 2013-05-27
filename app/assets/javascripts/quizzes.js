79962cce11885259c9c0e3e73b64021a2215e313
Elizabeth Thompson <ethompson@ancestry.com>
2013-05-26 21:06:15 -0700

    saved answers and updated new answers

--- app/assets/javascripts/quizzes.js
+++ app/assets/javascripts/quizzes.js
@@ -4,10 +4,59 @@ angular.module('Quiz', ['ngResource']).
     });
     
 function QuizCtrl($scope, $resource) {
+    var step = 0;
+    savedAnswers = [];
+    $scope.radios = {selected: 'X'};
     Question = $resource('/quizzes/show');
     questions = Question.query(function(){
-        $scope.question = questions[0].question
-        $scope.answers = questions[0].answers;
+        $scope.question = questions[step].question;
+        $scope.answers = questions[step].answers;
+        $scope.answers = ansArray();
+        removeExtras();
+        
     });
+    $scope.nextStep = function() {
+        step++;
+        $scope.question = questions[step].question;
+        saveAnswer(step);
+        $scope.answers = ansArray();
+        removeExtras();
+        
+    };
+    saveAnswer = function(step){
+        var radios = $scope.radios.selected
+        angular.forEach(radios, function(radios) {
+            if (radios) {
+                savedAnswers.push(radios);
+            }
+        });
+        
+    };
+    answersCount = function(step) {
+        var count = 0;
+        var answers = questions[step].answers;
+        angular.forEach(answers, function(answers){
+            count ++;
+        });
+        return count; 
+    };
+    ansArray = function(){
+        answers = [
+            {text:questions[step].answers[0], value:"A"},
+            {text:questions[step].answers[1], value:"B"},
+            {text:questions[step].answers[2], value:"C"},
+            {text:questions[step].answers[3], value:"D"},
+            {text:questions[step].answers[4], value:"E"},
+            {text:questions[step].answers[5], value:"F"}
+        ];
+        return answers;
+    }
+    removeExtras = function(){
+        count = answersCount(step)+4;
+        for (i=count;i<10;i++) {
+            var index=$scope.answers.indexOf($scope.answers[i])
+            $scope.answers.splice(index);
+        }
+    }
     
 }
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,11 +4,11 @@ class Quiz < ActiveRecord::Base
     belongs_to :user
     
     def self.question
-        [{"question"=>"Which is not an advantage of using a closure?","answers"=>{"answerA"=>"Prevent pollution of global scope", "answerB"=>"Encapsulation", "answerC"=>"Private properties and methods", "answerD"=>"Allow conditional use of 'strict mode'"}},
-        {"q2"=>"To create a columned list of twoline email subjects and dates for a masterdetail view, which is the most semantically correct?"},
-        {"q3"=>"To pass an array of strings to a function, you should not use..."},
-        {"q4"=>"____ and ____ would be the HTML tags you would use to display a menu item and its description."},
-        {"q5"=>"Given <div id=\"outer\"><div class=\"inner\"></div></div>, which of these two is the most performant way to select the inner div?"}]
+        [{"question"=>"Which is not an advantage of using a closure?","answers"=>["Prevent pollution of global scope","Encapsulation","Private properties and methods","Allow conditional use of 'strict mode'"]},
+        {"question"=>"To create a columned list of twoline email subjects and dates for a masterdetail view, which is the most semantically correct?","answers"=>["<div>+<span>", "<tr>+<td>", "<ul>+<li>", "<p>+<br>'", "none of these", "all of these"]},
+        {"question"=>"To pass an array of strings to a function, you should not use...","answers"=>["fn.apply(this, stringsArray)", "fn.call(this, stringsArray)", "fn.bind(this, stringsArray)"]},
+        {"question"=>"____ and ____ would be the HTML tags you would use to display a menu item and its description.","answers"=>["Prevent pollution of global scope", "Encapsulation", "Private properties and methods", "Allow conditional use of 'strict mode'"]},
+        {"question"=>"Given <div id=\"outer\"><div class=\"inner\"></div></div>, which of these two is the most performant way to select the inner div?","answers"=>["getElementById(\"outer\").children[0]", "getElementsByClassName(\"inner\")[0]"]}]
     end
 
 end
