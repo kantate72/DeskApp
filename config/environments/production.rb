6c86e9ba7e59fcff973a7480c370b0f763d52511
Elizabeth Thompson <ethompson@ancestry.com>
2013-05-27 21:32:42 -0700

    precompiled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ QuizApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( screen.css, angular.min.js, angular-resource.min.js )
+  config.assets.precompile += %w( screen.css angular.min.js angular-resource.min.js )
     
     
   # Disable delivery errors, bad email addresses will be ignored
